867021a4e9e6357dcba968e151848cef38e74de2
Akiranishimura <nissi20ch@gmail.com>
2024-06-12 10:47:31 +0900

    darkmode部分を一部変更

--- src/util/judgeTheme.ts
+++ src/util/judgeTheme.ts
@@ -1,17 +1,13 @@
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+// console.log(window.matchMedia)  
+
+// localStorage.removeItem('theme')
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
   } else {
     document.documentElement.classList.remove('dark')
   }
   
-  // Whenever the user explicitly chooses light mode
-  localStorage.theme = 'light'
-  
-  // Whenever the user explicitly chooses dark mode
-  localStorage.theme = 'dark'
-  
-  // console.log(localStorage.theme)
+ // console.log(localStorage.theme)
   
   // Whenever the user explicitly chooses to respect the OS preference
-  localStorage.removeItem('theme')
