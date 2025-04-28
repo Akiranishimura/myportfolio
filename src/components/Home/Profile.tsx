710618162e8211abe7c511a365d4ef025560520a
Akiranishimura <nissi20ch@gmail.com>
2025-04-29 00:15:45 +0900

    update: my information

--- src/components/Home/Profile.tsx
+++ src/components/Home/Profile.tsx
@@ -1,38 +1,18 @@
 export default function Profile() {
-
-    return (
-        <section>
-            <h2>
-                Profile
-            </h2>
-            <ul className="list-inside list-disc">
-                <li>
-                    西村朗
-                </li>
-                <li>
-                    21歳（2003年2月生まれ）
-                </li>
-                <li>
-                    東京理科大学創域理工学部電気電子情報工学科 4年
-                    <ul className="list-inside list-disc ml-4">
-                        <li>
-                        修士課程への進学を予定しています
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    片山研究室所属
-                </li>
-                <li>
-                    元東京理科大学野田地区理大祭実行委員会Web担当（2021-2023）
-                </li>
-                <li>
-                    趣味は自作キーボード
-                </li>
-            </ul>
-            <p className="mt-4">
-                西村朗と申します。自らの視点と技術で社会を良くしたいという思いから、誰かの役に立つサービスやプロダクトを作成したいと考えています。
-            </p>
-        </section>
-        
-    )}
+  return (
+    <section>
+      <h2>Profile</h2>
+      <ul className="list-inside list-disc">
+        <li>西村朗</li>
+        <li>22歳（2003年2月生まれ）</li>
+        <li>東京理科大学院 創域理工学研究科 電気電子情報工学選考 修士1年</li>
+        <li>片山研究室所属</li>
+        <li>元東京理科大学野田地区理大祭実行委員会Web担当（2021-2023）</li>
+        <li>趣味は自作キーボード</li>
+      </ul>
+      <p className="mt-4">
+        西村朗と申します。自らの視点と技術で社会を良くしたいという思いから、誰かの役に立つサービスやプロダクトを作成したいと考えています。
+      </p>
+    </section>
+  );
+}
